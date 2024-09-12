1651c75bb3403432859ef7394dbd92fe220d3ce9
Jay Freeman <jason.dean.freeman@icloud.com>
2024-09-12 10:33:09 -0500

    Coming soon and link update

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         the box of rocks will return.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://theboxofrocks.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Coming Soon . . .  
         </a>
       </header>
     </div>

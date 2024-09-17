90104ce4edb724bd2262a51562dedbe182354eea
Jay Freeman <jason.dean.freeman@icloud.com>
2024-09-16 22:37:23 -0500

    uhhh

--- src/App.js
+++ src/App.js
@@ -1,28 +1,3 @@
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         the box of rocks will return.
-        </p>
-        <a
-          className="App-link"
-          href="https://theboxofrocks.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Coming Soon . . .  
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
-
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
